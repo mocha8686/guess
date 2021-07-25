931d4954feb02b06181db9d474d891b4ab80432f
huecester <jericho.paulino@yahoo.com.ph>
2021-07-24 19:46:46 -0700

    py: tabs to spaces

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ ans = random.randint(1, 100)
 guess = -1
 score = 21
 while (guess != ans and score > 0):
-	try:
-		guess = int(input('Guess the number between 1 - 100: '))
-	except ValueError:
-		print('Not a number')
-		continue
+    try:
+        guess = int(input('Guess the number between 1 - 100: '))
+    except ValueError:
+        print('Not a number')
+        continue
 
-	if (guess > ans):
-		print('Lower')
-	elif (guess < ans):
-		print('Higher')
-	score -= 1
+    if (guess > ans):
+        print('Lower')
+    elif (guess < ans):
+        print('Higher')
+    score -= 1
 
 if (score > 0):
-	print('Correct')
-	print(f'Score: {score}')
+    print('Correct')
+    print(f'Score: {score}')
 else:
-	print(f'Answer: {ans}')
+    print(f'Answer: {ans}')
